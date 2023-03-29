ac0191f53bb8fce1fd5a9da254cc98cf1de7ff2d
Edvard <edvard@shalaev.com>
2023-03-29 16:42:32 +0300

    fix: added default url to API request function

--- src/sections/section_3/dataFetchingAndSaving/services/api.service.ts
+++ src/sections/section_3/dataFetchingAndSaving/services/api.service.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 import { ProductList } from '../models/products.interfaces';
 
 const fetchProductList = async (): Promise<ProductList> => {
-  const apiUrl = process.env.PRODUCTS_URL || '';
+  const apiUrl =
+    process.env.PRODUCTS_URL ||
+    'https://raw.githubusercontent.com/Exove/developer-test/main/material/products.json';
   return (await axios.get(apiUrl)).data;
 };
 
